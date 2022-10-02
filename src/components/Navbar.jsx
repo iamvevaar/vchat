f69e10b27e31835248b139768b9422c6fadb540d
iamvevaar <gautam15suthar@gmail.com>
2022-10-03 03:28:32 +0530

    minor ui changes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,21 +1,10 @@
 import React from 'react'
-// import {signOut} from "firebase/auth"
-// import { auth } from '../firebase'
-// import { useContext } from 'react'
-// import { AuthContext } from '../context/AuthContext'
-
 
 const Navbar = () => {
-  // const {currentUser} = useContext(AuthContext);
 
   return (
     <div className='navbar'>
       <span className='logo'>VCHAT</span>
-      {/* <div className='user'>
-        <img src={currentUser.photoURL} alt="" />
-        <span>{currentUser.displayName}</span>
-        <button onClick={()=>signOut(auth)}>Logout</button>
-      </div> */}
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,17 +1,18 @@
-import React from 'react'
-import Navbar from './Navbar'
-import Search from './Search'
-import Chats from './Chats'
+import React from "react";
+import Navbar from "./Navbar";
+import Search from "./Search";
+import Chats from "./Chats";
 
 const Sidebar = () => {
   return (
-    <div className='sidebar'>
-      <Navbar/>
-      <Search/>
-      <Chats/>
+    <div className="sidebar">
+     
+        <Navbar />
+        <Search />     
+        <Chats />
+      
     </div>
+  );
+};
 
-    )
-}
-
-export default Sidebar
+export default Sidebar;
